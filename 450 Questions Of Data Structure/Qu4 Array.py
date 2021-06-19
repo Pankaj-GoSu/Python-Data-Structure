e9fc71a62023868862f35c50e6c2247015fea7d9
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-06-19 23:38:18 +0530

    Added comment in Qu4

--- 450 Questions Of Data Structure/Qu4 Array.py
+++ 450 Questions Of Data Structure/Qu4 Array.py
@@ -28,5 +28,45 @@ class Sorting:
             self.kth_min_element(list,N)
         return self.asceding_list
 x = Sorting()
+print("method 1st")
 list1 = [0,2,1,0,1]
 print(x.kth_min_element(list1,5))
+
+
+# method 2 
+
+def sort012(arr,n):
+    count_0 = 0
+    count_1 = 0
+    count_2 = 0
+    for item in arr:
+        if item == 0:
+            count_0 += 1
+        elif item == 1:
+            count_1 += 1
+        else:
+            count_2 += 1
+    i = 0
+    while count_0 or count_1 or count_2:
+        if count_0:
+            arr[i] = 0
+            i += 1
+            count_0 -= 1
+            continue 
+        elif count_1:
+            arr[i] = 1
+            i += 1
+            count_1 -= 1
+            continue 
+        else:
+            arr[i] = 2
+            i += 1
+            count_2 -= 1
+            continue 
+
+list1 = [0,2,1,0,2]
+sort012(list1,5) # jo argument hum pass kringe usme ye operation perform krega and then hume wo 
+                 # aur us argument m function k operation k hisab se change milinge jaise list1 
+                 # m change ho jayega wo sorted hoga.
+print("method 2nd")
+print(list1)
