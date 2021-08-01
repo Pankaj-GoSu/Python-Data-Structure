df2c08054becf84e2869d577e4ea9ff3414608df
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-08-01 20:48:31 +0530

    image to text code added

---
+++ Trial folder/creating-imagetotext.py
@@ -0,0 +1,16 @@
+import pywhatkit
+
+pywhatkit.image_to_ascii_art("Lattu.jpeg","Lattu")
+
+
+exit()
+import pywhatkit
+
+pywhatkit.image_to_ascii_art("AaYu.jpeg","AaYu.txt")
+
+
+
+exit()
+import pywhatkit
+
+pywhatkit.image_to_ascii_art("ChaSmu.jpeg","ChaSmu.txt")
